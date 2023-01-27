ca10a8dfd12b87f21c6dca6d3ce753ce591ad313
Arpitha Rajeev <108493146+Arpitha-Rajeev1@users.noreply.github.com>
2023-01-27 14:17:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
+      </BrowserRouter>
     </div>
   );
 }
